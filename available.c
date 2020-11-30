7bc56899b378d5fc0eafdf81aa6a832804abc14d
WenyuanShao <shaowy@gwmail.gwu.edu>
2020-11-30 21:03:58 +0800

    update layout

--- available.c
+++ available.c
@@ -3,62 +3,36 @@
 int
 main(void)
 {
-   int status;
-   int pkey;
-   int *buffer;
+	int status;
+	int pkey;
+	int *buffer;
 
-   /*
-    *Allocate one page of memory
-    */
-   buffer = mmap(NULL, getpagesize(), PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
-   if (buffer == MAP_FAILED)
-       errExit("mmap");
+	buffer = mmap(NULL, getpagesize(), PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
+	if (buffer == MAP_FAILED)
+		errExit("mmap");
 
-   /*
-    * Put some random data into the page (still OK to touch)
-    */
-   *buffer = __LINE__;
-   printf("buffer contains: %d\n", *buffer);
+	*buffer = __LINE__;
+	printf("buffer contains: %d\n", *buffer);
 
-   /*
-    * Allocate a protection key:
-    */
-  //unsigned int test = test_rdpkru();
-  //printf("test: %d\n", test);
-  pkey = _pkey_alloc();
-  printf("pkey: %d\n", pkey);
-   if (pkey == -1)
-       errExit("pkey_alloc");
+	unsigned int test = test_rdpkru();
+	printf("test: %d\n", test);
+	pkey = _pkey_alloc();
+	printf("pkey: %d\n", pkey);
+	if (pkey == -1)
+		errExit("pkey_alloc");
 
-   /*
-    * Disable access to any memory with "pkey" set,
-    * even though there is none right now
-    */
-   _pkey_set(pkey, PKEY_DISABLE_ACCESS, 0);
-   //if (status)
-   //    errExit("pkey_set");
+	_pkey_set(pkey, PKEY_DISABLE_ACCESS, 0);
 
-   /*
-    * Set the protection key on "buffer".
-    * Note that it is still read/write as far as mprotect() is
-    * concerned and the previous pkey_set() overrides it.
-    */
-   status = _pkey_mprotect(buffer, getpagesize(),
-                          PROT_READ | PROT_WRITE, pkey);
-   //if (status == -1)
-   //    errExit("pkey_mprotect");
+	status = _pkey_mprotect(buffer, getpagesize(), PROT_READ | PROT_WRITE, pkey);
 
-   printf("about to read buffer again...\n");
+	printf("about to read buffer again...\n");
 
-   /*
-    * This will crash, because we have disallowed access
-    */
-   printf("buffer contains: %d\n", *buffer);
+	printf("buffer contains: %d\n", *buffer);
 
-   status = _pkey_free(pkey);
-   if (status == -1)
-       errExit("pkey_free");
+	status = _pkey_free(pkey);
+	if (status == -1)
+		errExit("pkey_free");
 
-   exit(EXIT_SUCCESS);
-    return 0;
+	exit(EXIT_SUCCESS);
+	return 0;
 }
