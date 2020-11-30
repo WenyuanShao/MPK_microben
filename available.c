fcc9d037b141aeccdf691f837b90040c155d1cfa
WenyuanShao <shaowy@gwmail.gwu.edu>
2020-11-30 20:56:01 +0800

    update test version2

--- available.c
+++ available.c
@@ -1,67 +1,64 @@
-#include <unistd.h>
-#include <sys/syscall.h>
-#include <stdio.h>
-#include <sys/mman.h>
-#include <stdlib.h>
-#include <assert.h>
 #include "MPK_helper.h"
 
 int
 main(void)
 {
-	int          status;
-	int          pkey;
-	unsigned int rdkey;
-	int         *buffer;
+   int status;
+   int pkey;
+   int *buffer;
 
-	/*
-	 *Allocate one page of memory
-	 */
-	buffer = mmap(NULL, getpagesize(), PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
-	if (buffer == MAP_FAILED)
-		errExit("mmap");
+   /*
+    *Allocate one page of memory
+    */
+   buffer = mmap(NULL, getpagesize(), PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
+   if (buffer == MAP_FAILED)
+       errExit("mmap");
 
-	*buffer = __LINE__;
-	printf("buffer contains: %d\n", *buffer);
+   /*
+    * Put some random data into the page (still OK to touch)
+    */
+   *buffer = __LINE__;
+   printf("buffer contains: %d\n", *buffer);
 
-	/*
-	 * Allocate a protection key:
-	 */
-	rdkey= test_rdpkru();
-	printf("rdpkru: 0x%x\n", rdkey);
+   /*
+    * Allocate a protection key:
+    */
+  //unsigned int test = test_rdpkru();
+  //printf("test: %d\n", test);
+  pkey = _pkey_alloc();
+  printf("pkey: %d\n", pkey);
+   if (pkey == -1)
+       errExit("pkey_alloc");
 
-	pkey = _pkey_alloc();
-	printf("pkey: %d\n", pkey);
+   /*
+    * Disable access to any memory with "pkey" set,
+    * even though there is none right now
+    */
+   _pkey_set(pkey, PKEY_DISABLE_ACCESS, 0);
+   //if (status)
+   //    errExit("pkey_set");
 
-	if (pkey == -1)
-		errExit("pkey_alloc");
+   /*
+    * Set the protection key on "buffer".
+    * Note that it is still read/write as far as mprotect() is
+    * concerned and the previous pkey_set() overrides it.
+    */
+   status = _pkey_mprotect(buffer, getpagesize(),
+                          PROT_READ | PROT_WRITE, pkey);
+   //if (status == -1)
+   //    errExit("pkey_mprotect");
 
-	/*
-	 * Disable access to any memory with "pkey" set,
-	 * even though there is none right now
-	 */
-	_pkey_set(pkey, PKEY_DISABLE_ACCESS, 0);
+   printf("about to read buffer again...\n");
 
-	/*
-     * Set the protection key on "buffer".
-	 * Note that it is still read/write as far as mprotect() is
-	 * concerned and the previous pkey_set() overrides it.
-	 */
-	status = _pkey_mprotect(buffer, getpagesize(), PROT_READ | PROT_WRITE, pkey);
-	if (status == -1)
-		errExit("pkey_mprotect");
+   /*
+    * This will crash, because we have disallowed access
+    */
+   printf("buffer contains: %d\n", *buffer);
 
-	printf("about to read buffer again...\n");
+   status = _pkey_free(pkey);
+   if (status == -1)
+       errExit("pkey_free");
 
-	/*
-	 * This will crash, because we have disallowed access
-	 */
-	printf("buffer contains: %d\n", *buffer);
-
-	status = _pkey_free(pkey);
-	if (status == -1)
-		errExit("pkey_free");
-
-	exit(EXIT_SUCCESS);
-	return 0;
+   exit(EXIT_SUCCESS);
+    return 0;
 }
