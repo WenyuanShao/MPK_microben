0379d8e677c1df23ee0fb2efd8f23db7d5b77487
WenyuanShao <shaowy@gwmail.gwu.edu>
2020-12-07 17:20:49 +0800

    update a callget test

--- multi_process.c
+++ multi_process.c
@@ -5,7 +5,7 @@ int
 main(void)
 {
 	int status;
-	int pkey;
+	int skey, ckey;
 	int *buffer;
 	int pid;
 
@@ -16,31 +16,23 @@ main(void)
 	*buffer = __LINE__;
 	printf("buffer contains: %d\n", *buffer);
 	
-	pid = fork();
-	assert(pid >= 0);
-	if (pid != 0) {
-		pkey = _pkey_alloc();
-		printf("pkey: %d\n", pkey);
-		if (pkey == -1)
-			errExit("pkey_alloc");
+	skey = _pkey_alloc();
+	ckey = _pkey_alloc();
+	printf("pkey: %d\n", skey);
+	printf("pkey: %d\n", ckey);
+	/*if (pkey == -1)
+		errExit("pkey_alloc");
 
-		_pkey_set(pkey, 0, 0);
+	_pkey_set(pkey, 0, 0);
 
-		status = _pkey_mprotect(buffer, getpagesize(), PROT_READ | PROT_WRITE, pkey);
+	status = _pkey_mprotect(buffer, getpagesize(), PROT_READ | PROT_WRITE, pkey);
 
-		//printf("about to read buffer again...\n");
+	//printf("about to read buffer again...\n");
 
-		//status = _pkey_free(pkey);
-		//if (status == -1)
-		//	errExit("pkey_free");
-	} else {
-		pkey = _pkey_alloc();
-		printf("pkey: %d\n", pkey);
-		_pkey_set(pkey, 0, 0);
-		/*while(1) {
-			printf("buffer contains: %d\n", *buffer);
-		}*/	
-	}
+	//status = _pkey_free(pkey);
+	//if (status == -1)
+	//	errExit("pkey_free");
+	}*/
 	exit(EXIT_SUCCESS);
 	return 0;
 }
