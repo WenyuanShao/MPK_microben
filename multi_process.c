8b7455dd49c3ea1b50f1c6e5af3bcc00f824b39f
WenyuanShao <shaowy@gwmail.gwu.edu>
2020-12-07 20:34:31 +0800

    update test

--- multi_process.c
+++ multi_process.c
@@ -7,7 +7,6 @@ main(void)
 	int status;
 	int skey, ckey;
 	int *buffer;
-	int pid;
 
 	buffer = mmap(NULL, getpagesize(), PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
 	if (buffer == MAP_FAILED)
@@ -17,22 +16,22 @@ main(void)
 	printf("buffer contains: %d\n", *buffer);
 	
 	skey = _pkey_alloc();
+	assert(skey > 0);
 	ckey = _pkey_alloc();
+	assert(ckey > 0);
 	printf("pkey: %d\n", skey);
 	printf("pkey: %d\n", ckey);
-	/*if (pkey == -1)
-		errExit("pkey_alloc");
 
-	_pkey_set(pkey, 0, 0);
+	_pkey_set(skey, 0, 0);
+	status = _pkey_mprotect(buffer, getpagesize(), PROT_READ | PROT_WRITE, skey);
+	printf("read buffer: %d\n", *buffer);
 
-	status = _pkey_mprotect(buffer, getpagesize(), PROT_READ | PROT_WRITE, pkey);
-
-	//printf("about to read buffer again...\n");
-
+	_pkey_set(ckey, 0, 0);
+	//status = _pkey_mprotect(buffer, getpagesize(), PROT_READ | PROT_WRITE, skey);
+	printf("read buffer again: %d\n", *buffer);
 	//status = _pkey_free(pkey);
 	//if (status == -1)
 	//	errExit("pkey_free");
-	}*/
 	exit(EXIT_SUCCESS);
 	return 0;
 }
