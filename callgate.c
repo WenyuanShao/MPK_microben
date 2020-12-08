ed0a098c2cb314970bf35d2cb374abde5e94d05e
WenyuanShao <shaowy@gwmail.gwu.edu>
2020-12-08 12:05:24 +0800

    update test

--- callgate.c
+++ callgate.c
@@ -69,8 +69,10 @@ main(void)
 	unsigned int rdpkru = test_rdpkru();
 	printf("rdpkru: 0x%x\n", rdpkru);
 
-	//_pkey_set(ckey, 0, 0);
-	//_pkey_set(skey, PKEY_DISABLE_ACCESS, 0);
+	_pkey_set(ckey, 0, 0);
+	_pkey_set(skey, PKEY_DISABLE_ACCESS, 0);
+	unsigned int rdpkru = test_rdpkru();
+	printf("rdpkru: 0x%x\n", rdpkru);
 	printf("read buffer again: %d\n", *buffer);
 	exit(EXIT_SUCCESS);
 	return 0;
