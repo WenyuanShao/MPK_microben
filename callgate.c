1e29038bd1e6055147cf8941e3cd10e3bbb47173
WenyuanShao <shaowy@gwmail.gwu.edu>
2020-12-11 12:10:00 +0800

    update test

--- callgate.c
+++ callgate.c
@@ -76,7 +76,7 @@ init(int *key) {
 	status = _pkey_mprotect(buffer, getpagesize(), PROT_READ | PROT_WRITE, *key);
 	assert(status >= 0);
 	_pkey_set(*key, PKEY_DISABLE_ACCESS, 0);
-	printf("init read buffer: %d\n", *buffer);
+	//printf("init read buffer: %d\n", *buffer);
 
 	return buffer;
 }
