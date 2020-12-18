a15fe3472f3c57aa5db0eefb4c3b49d8246d34b8
WenyuanShao <shaowy@gwmail.gwu.edu>
2020-12-18 13:44:25 +0800

    test

--- callgate.c
+++ callgate.c
@@ -66,7 +66,7 @@ callgate()
 						 //"wrpkru\n\t"
 						 //push into stack
 						 "movq $tid, %%rax\n\t"
-						 "shl $0x7, %%rax\n\t"
+						// "shl $0x7, %%rax\n\t"
 						 //"add $s, %%rax\n\t"
 						 //"movq $tid, %%rdx\n\t"
 						 //"shl $0x3, %%rdx\n\t"
