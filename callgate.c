704d896e77eb1d10bef5c6892ad7a4673a392b15
WenyuanShao <shaowy@gwmail.gwu.edu>
2020-12-18 13:10:48 +0800

    test

--- callgate.c
+++ callgate.c
@@ -52,7 +52,6 @@ callgate()
 	//unsigned int pkru = (0 << (2 * S_KEY));
 	//printf("pkru: 0x%x\n", pkru);
 
-	unsigned long long a;
 	/* 
 	 * Composite version should rely on stubs to save current state of
 	 * a thread. As a result, in this prototype, I don't consider it.
@@ -66,10 +65,8 @@ callgate()
 						 //"wrpkru\n\t"
 						 //push into stack
 						 "movq $tid, %%rax\n\t"
-
+						 "shl $0x7, %%rax\n\t"
 						 "movq (%%rax), %%rax\n\t"
-						 //"shl $0x7, %%rax\n\t"
-						 //"movq (%%rax), %%rax\n\t"
 						 //"movq $tid, %%rdx\n\t"
 						 //"shl $0x3, %%rdx\n\t"
 						 //"add %%rdx, %%rax\n\t"
@@ -79,9 +76,9 @@ callgate()
 						 //"movq (%%rax), %%rcx\n\t"
 						 //"movq %%rsp, 0x8(%%rax, %%rcx, 16)\n\t"
 						 //done
-					//	 "xor %%rcx, %%rcx\n\t"
-					//	 "xor %%rdx, %%rdx\n\t"
-						// "movl $pkru_callee, %%eax\n\t"
+						 "xor %%rcx, %%rcx\n\t"
+						 "xor %%rdx, %%rdx\n\t"
+						 "movl $pkru_callee, %%eax\n\t"
 						// "wrpkru\n\t"
 						// "cmp $token, %%r15\n\t"
 						// "jne 1f\n\t"
@@ -91,11 +88,10 @@ callgate()
 						// "call callgate_abuse\n\t"
 						// "2:"
 						 //: [caller_addr] "=rm" (caller_addr)
-						 : "=a" (a)
+						 :
 						 :
 						 :);
 	printf("test: %llu\n", s[tid].r[0].sp);
-	printf("a: %llu\n", a);
 }
 
 int
