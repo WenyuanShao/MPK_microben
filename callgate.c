32bb3eceaf8e7e396d6e07e91b660efac0089391
WenyuanShao <shaowy@gwmail.gwu.edu>
2020-12-23 05:18:04 -0500

    test

--- callgate.c
+++ callgate.c
@@ -45,9 +45,7 @@ callgate()
 	 * a thread. As a result, in this prototype, I don't consider it.
 	 */
 	unsigned long long end, start;
-	int i = 0;
 	start = mpk_tsc();
-	for (i = 0; i < 100; i++) {
 	__asm__ __volatile__("movq $0xfffffffffffffff0, %%r15\n\t" // caller_token
 	                     "xor %%rcx, %%rcx\n\t"
 	                     "xor %%rdx, %%rdx\n\t"
@@ -108,7 +106,6 @@ callgate()
 	                     : "=r" (verifier)
 	                     :
 	                     : "memory", "cc");
-	}
 	end = mpk_tsc();
 	printf("overhead: %llu\n", (end-start)/100);
 }
