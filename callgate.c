e3fc5b818a5a6d83e6e0e0e20b14c525754976b4
WenyuanShao <shaowy@gwmail.gwu.edu>
2020-12-23 05:06:37 -0500

    test

--- callgate.c
+++ callgate.c
@@ -16,8 +16,8 @@ struct stack {
 int pkey[2];
 static unsigned long long token  = ~(unsigned long long)0;
 static unsigned long pkru_userlevel_kernel = 0;
-static unsigned long pkru_callee = 0;
-static unsigned long pkru_caller = 0;
+static unsigned long pkru_callee = 0xfffffff0;
+static unsigned long pkru_caller = 0xffffffcc;
 static unsigned long long tid    = 0;
 static struct stack s[8];
 unsigned long long verifier;
@@ -46,7 +46,7 @@ callgate()
 	 */
 	unsigned long long end, start;
 	start = mpk_tsc();
-	/*__asm__ __volatile__("movq $0xfffffffffffffff0, %%r15\n\t" // caller_token
+	__asm__ __volatile__("movq $0xfffffffffffffff0, %%r15\n\t" // caller_token
 	                     "xor %%rcx, %%rcx\n\t"
 	                     "xor %%rdx, %%rdx\n\t"
 	                     "movq %%rsp, %0\n\t"
@@ -75,7 +75,7 @@ callgate()
 	                     "1:\n\t"
 	                     "call callgate_abuse\n\t"
 	                     "2:\n\t"
-	                     "movq $0xfffffffffffffff1, %%r15\n\t" // callee_token
+	                     /*"movq $0xfffffffffffffff1, %%r15\n\t" // callee_token
 	                     "xor %%rcx, %%rcx\n\t"
 	                     "xor %%rdx, %%rdx\n\t"
 	                     "movl $pkru_userlevel_kernel, %%eax\n\t"
@@ -105,9 +105,9 @@ callgate()
 	                     "4:"
 	                     : "=r" (verifier)
 	                     :
-	                     : "memory", "cc");
+	                     : "memory", "cc");*/
 	end = mpk_tsc();
-	printf("overhead: %llu\n", end-start);*/
+	printf("overhead: %llu\n", end-start);
 }
 
 int
@@ -133,14 +133,14 @@ void
 client_call(int *s_buffer)
 {
 	callgate();
-	//printf("expr read buffer: %d\n", *s_buffer);
+	printf("expr read buffer: %d\n", *s_buffer);
 }
 
 int
 main(void)
 {
 	int *s_buffer, *c_buffer;
-	unsigned long pkru = 0xffffffff;
+	unsigned long pkru = 0xfffffffc;
 
 	printf("PKEY_DISABLE_ACCESS:%d\n", PKEY_DISABLE_ACCESS);
 	pkey[0] = init(&s_buffer);
@@ -148,12 +148,12 @@ main(void)
 	//printf("pkru: %x, pkey: %d\n", pkru, pkey[0]);
 	pkey[1] = init(&c_buffer);
 	wrpkru(pkru);
-	//pkru = test_rdpkru();
-	//printf("pkru: %x, pkey: %d\n", pkru, pkey[1]);
+	pkru = test_rdpkru();
+	printf("pkru: %x, pkey: %d\n", pkru, pkey[1]);
 	//printf("expr read buffer: %d\n", *s_buffer);
 
 	//_pkey_set(pkey[0], PKEY_DISABLE_ACCESS, 0);
-	//client_call(s_buffer);
+	client_call(s_buffer);
 
 	return 0;
 }
